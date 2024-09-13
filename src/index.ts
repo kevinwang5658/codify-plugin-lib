d0b7e5b36bb1ebbefe229248fff76df1c16e1d31
kevinwang <kevinwang5658@gmail.com>
2024-09-12 23:35:20 -0400

    Improved exports and removed unused code

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,20 @@ export * from './plan/change-set.js'
 export * from './plan/plan.js'
 export * from './plan/plan-types.js'
 export * from './plugin/plugin.js'
-export { ParsedResourceSettings } from './resource/parsed-resource-settings.js';
+export * from './resource/parsed-resource-settings.js';
 export * from './resource/resource.js'
-export * from './resource/resource-settings.js'
-
+export {
+  ArrayParameterSetting,
+  DefaultParameterSetting,
+  ParameterSetting,
+  ParameterSettingType,
+  ResourceSettings,
+  StatefulParameterSetting
+} from './resource/resource-settings.js'
 export * from './resource/stateful-parameter.js'
+export * from './utils/utils.js'
 
 export async function runPlugin(plugin: Plugin) {
   const messageHandler = new MessageHandler(plugin);
   process.on('message', (message) => messageHandler.onMessage(message))
 }
-
-export * from './utils/utils.js'
